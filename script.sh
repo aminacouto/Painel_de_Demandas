4d3c87db44e197757554e85423ff61ebe2a14b56
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2025-03-20 12:18:28 -0300

    update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,13 @@
 
 echo "Inicializando o aplicativo LAD..."
 
+# Lendo o token do usuário
+echo "Por favor, insira seu token do GitHub:"
+read GITHUB_TOKEN
+
+# Exportando o token como variável de ambiente
+export GITHUB_TOKEN="$GITHUB_TOKEN"
+
 # Instalando as dependências
 echo "Instalando as bibliotecas necessárias..."
 pip install Flask dash pandas matplotlib requests
