89575f1f9cebaa211c0f3eb316000abab991dfb4
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2025-03-20 10:40:59 -0300

    config GITHUB_TOKEN no Script

--- script.sh
+++ script.sh
@@ -2,11 +2,13 @@
 
 echo "Inicializando o aplicativo LAD..."
 
+# Configurando o GITHUB_TOKEN
+echo "Configurando o token de autenticação..."
+export GITHUB_TOKEN=$SECRET_TOKEN
+
 # Instalando as dependências
 echo "Instalando as bibliotecas necessárias..."
 pip install Flask dash pandas matplotlib requests
-
-echo "Instalação das bibliotecas concluída."
 
 # Navegando para o diretório onde o app.py está localizado
 echo "Navegando para o diretório do aplicativo..."
