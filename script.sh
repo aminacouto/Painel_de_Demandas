de350ba82536fd99b76aef6abd19c8c425909b57
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2025-03-20 11:15:43 -0300

    automatização token

--- script.sh
+++ script.sh
@@ -2,6 +2,10 @@
 
 echo "Inicializando o aplicativo LAD..."
 
+# Executa o script de configuração do token
+echo "Configurando o Token do GitHub..."
+bash setup_token.sh
+
 # Instalando as dependências
 echo "Instalando as bibliotecas necessárias..."
 pip install Flask dash pandas matplotlib requests
