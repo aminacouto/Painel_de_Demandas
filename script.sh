5f1deeb755b58b2d3123e8e9b8960ad86a13ad19
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2025-03-20 11:56:36 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,17 +2,9 @@
 
 echo "Inicializando o aplicativo LAD..."
 
-# Executa o script de configuração do token
-echo "Configurando o Token do GitHub..."
-bash setup_token.sh
-
 # Instalando as dependências
 echo "Instalando as bibliotecas necessárias..."
 pip install Flask dash pandas matplotlib requests
-
-# Navegando para o diretório onde o app.py está localizado
-echo "Navegando para o diretório do aplicativo..."
-cd source  # Se o diretório for outro, ajuste conforme necessário
 
 # Executando o aplicativo Dash
 echo "Executando o aplicativo..."
